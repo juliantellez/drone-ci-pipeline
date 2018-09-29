cd2b51f1a0814941ba9ea1f5b97539b4b9cbf04a
Julian Tellez <juliantellezmendez@gmail.com>
2018-09-29 23:24:30 +0100

    checking agents

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,7 @@ DRONE_COMMIT_MESSAGE=$1
 # SEMVER
 SEMVER=$(echo ${DRONE_COMMIT_MESSAGE} | awk 'match(tolower($0),/(minor|major)/) {print substr(tolower($0),RSTART,RLENGTH)}')
 
+
 if [ $SEMVER ]; then
     RELEASE_VERSION=$SEMVER; else
     RELEASE_VERSION=patch
