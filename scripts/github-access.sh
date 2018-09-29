643f6d075095fa8f32739561e1561b165ba2154a
Julian Tellez <juliantellezmendez@gmail.com>
2018-09-29 23:38:23 +0100

    amending publish script

---
+++ scripts/github-access.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ -z "${ID_RSA}" ]; then
+    echo "ERROR: No ID_RSA file detected."
+    exit 1
+fi
+
+# GitHub access
+mkdir -p ~/.ssh/
+echo "$ID_RSA" > ~/.ssh/id_rsa
+
+chmod 0600  ~/.ssh/id_rsa
+
+# Accept github domain
+touch ~/.ssh/known_hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# Global configuration
+git config --global url.ssh://git@github.com/.insteadOf https://github.com/
+
+git config --global push.default simple
+git config --global user.email "juliantellezmendez@gmail.com"
+git config --global user.name "Julian Tellez"
+
+ssh-keygen -y -e -f ~/.ssh/id_rsa
+
+echo "\n\n Verify server: \n\n"
+ssh -vT git@github.com
+
+echo "\n\n Verify user: \n\n"
+ssh -T git@github.com
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,13 +10,10 @@ if [ $SEMVER ]; then
     RELEASE_VERSION=patch
 fi
 
+echo "Release : ${RELEASE_VERSION}"
+
 # PUBLISH
 npm version $RELEASE_VERSION -m "[CI SKIP] Publish player v%s"
-
-# UPDATE REMOTE
-git config --global push.default simple
-git config user.email "bot@djuliantellez.com"
-git config user.name "[ PLAYBACK BOT ] DAZN"
 
 git push origin master --no-verify
 git push origin --tags --no-verify
